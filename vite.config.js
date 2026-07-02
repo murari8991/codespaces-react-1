11c796077ac5256f1cc187950f514ec1d26b9d04
That Other Dude <murari2lakshman.ponduru@gmail.com>
2026-07-02 17:05:04 +0000

    Added url for the website

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-=======
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 140f7ee46e3680335efb09c81ef605cb21207e9f
   test: {
     globals: true,
     environment: 'jsdom',
